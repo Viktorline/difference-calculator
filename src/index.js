af3e9467a573b1c543218dbdbf14d77f41501e32
Victor Murzin <viktorlinework@gmail.com>
2022-11-22 22:54:23 +0700

    add buildFullPath func

--- src/index.js
+++ src/index.js
@@ -4,12 +4,15 @@ import difference from './diff.js';
 import getFile from './parser.js';
 import format from './formatters/index.js';
 
-const readFile = (pathToFile) => fs.readFileSync(path.resolve(process.cwd(), pathToFile));
+const buildFullPath = (pathToFile) => path.resolve(process.cwd(), pathToFile);
+const readFile = (fullPathToFile) => fs.readFileSync(fullPathToFile);
 const getFileExtension = (pathToFile) => path.extname(pathToFile).slice(1);
 
 export default (pathToFile1, pathToFile2, formatter = 'stylish') => {
-  const file1 = getFile(readFile(pathToFile1), getFileExtension(pathToFile1));
-  const file2 = getFile(readFile(pathToFile2), getFileExtension(pathToFile2));
+  const fullPathFile1 = buildFullPath(pathToFile1);
+  const fullPathFile2 = buildFullPath(pathToFile2);
+  const file1 = getFile(readFile(fullPathFile1), getFileExtension(fullPathFile1));
+  const file2 = getFile(readFile(fullPathFile2), getFileExtension(fullPathFile2));
   const differenceTree = difference(file1, file2);
   const result = format(differenceTree, formatter);
   return result;
